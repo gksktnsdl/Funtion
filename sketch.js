c1573bee1833096d13595a1ac2573939c0ced88c
gksktnsdl <gksktnsdl@naver.com>
2014-10-16 15:57:47 +0900

    c

--- sketch.js
+++ sketch.js
@@ -11,19 +11,22 @@ function draw()
   fill(255);
   ellipse(mouseX,mouseY,100,100);
 
-  drawDonut1();
+  drawDonut1(100,100);
+  drawDonut1(300,100);
+  drawDonut1(400,200);
+  drawDonut1(500,300);
 }
 
 
 
 
-function drawDonut1()
+function drawDonut1(x,y)
 { 
     
    fill(255);
-   ellipse(300,300,200,200);
+   ellipse(x,y,200,200);
    fill(0);
-   ellipse(300,300,120,120);
+   ellipse(x,y,120,120);
 
 
    
