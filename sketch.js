88d89c6086e31a73e6d57845ee01498b8552ac95
gksktnsdl <gksktnsdl@naver.com>
2014-10-16 15:43:43 +0900

    t

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 function setup()
 {
   createCanvas(800,600);
-  noFill();
-  stroke(255,0,0);
+  fill(255);
+  stroke(255);
 
 }
 
 function draw()
 {
   background(0);
-  ellipse(400,300,mouseX,mouseY);
+  ellipse(400,300,300,300);
 }
 
 
